eb02aa71dc6c3fe9a2b5b86dbec94508c6b9cc66
Avery Radmacher <45777186+avery-radmacher@users.noreply.github.com>
2020-10-01 17:09:45 -0400

    Create html subsection (#1)
    * Add class HtmlSubsection
    
    * Add string to html subsection method
    
    * Move methods from String to HtmlSubsection
    
    * Use method chaining
    
    * Allow initialize to take string or array
    
    * improve get_block and get_text
    
    * pick out text in article better
    
    * Improve text cleaning
    
    * Add get_author
    
    * Add get_title

--- scraper.rb
+++ scraper.rb
@@ -1,27 +1,72 @@
 require 'eat'
 
 class String
-    def get_block pattern, occurence = 1
-        self.scan(pattern) {|block|
-            if occurence == 1
-                return block
-            end
-            occurence -= 1
+    def to_HtmlSubsection
+        return HtmlSubsection.new self
+    end
+end
+
+class HtmlSubsection
+    def initialize texts = nil
+        @subtexts = []
+        if texts.kind_of? Array
+            @subtexts = texts
+        elsif texts.kind_of? String
+            @subtexts << texts.to_s
+        end
+    end
+    
+    def get_block pattern, occurence = -1
+        newSubtexts = []
+        @subtexts.each {|subtext|
+            subtext.scan(pattern) {|block|
+                if occurence == 1 || occurence == -1
+                    newSubtexts << block
+                end
+                if occurence != -1
+                    occurence -= 1
+                end
+            }
         }
-        return ""
+        return HtmlSubsection.new newSubtexts
+    end
+
+    def get_title
+        match = @subtexts[0].match(/\<a.*?\>(.*?)\<\/a\>/)
+        return match[1]
+    end
+
+    def get_author
+        match = @subtexts[0].match(/\<article.*?\>/)
+        match = match[0].match(/hentry author.*? /)
+        return match[0].gsub(/(hentry author-|-| )/, "hentry author-" => "", "-" => " ", " " => "")
     end
 
     def get_text
         text = ""
-        self.scan(/(?<=\>).*?(?=\<)/) {|paragraph|
-            text += paragraph + "\n"
+        @subtexts.each {|subtext|
+            subtext.scan(/(?<="white-space:pre-wrap;"\>).*?(?=\<\/p)/) {|paragraph|
+                text += paragraph.gsub(/\<\/?(strong|em|a|span).*?\/?\>/, "") + "\n"
+            }
         }
         return text
+            .gsub(/\n+/, "\n\n")
+            .gsub(/(&nbsp;| )+/, " ")
     end
 end
 
+#source = eat('https://www.bagpipeonline.com/opinions/2020/9/12/prevent-a-twindemic-get-a-vaccine')
 source = eat('https://www.bagpipeonline.com/news/2020/9/15/policy-overview-of-presidential-candidates')
+    .to_HtmlSubsection
 #block = source.get_block(/\<head\>.*\<\/head\>/m)
-block = source.get_block(/\<article class=.*?\<\/article\>/m)
-block = block.get_block(/\<div class="sqs-block-content"\>.*?\<\/div\>/m, 2)
-puts block.get_text
+article = source
+    .get_block(/\<article class=.*?\<\/article\>/m)
+author = article.get_author
+title = article.get_title
+articleInfo = article
+    .get_block(/\<header\>.*?\<\/header\>/m)
+articleText = article
+    .get_block(/\<div class="sqs-block-content"\>.*?\<\/div\>/m)
+puts title
+puts "by #{author}\n\n"
+puts articleText.get_text
